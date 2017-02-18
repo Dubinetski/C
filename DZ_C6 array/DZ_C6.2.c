b02f774f392e8a4ee9760b641a287d081d4942f9
Sergey Gradovich <introkun@gmail.com>
2017-02-18 14:07:23 +0300

    Update DZ_C6.2.c

--- DZ_C6 array/DZ_C6.2.c
+++ DZ_C6 array/DZ_C6.2.c
@@ -1,4 +1,4 @@
-﻿/*
+/*
 Задание 2
 Найти сумму положительных элементов двумерного массива.
 */
@@ -7,19 +7,21 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define ARRAY_ROW 5                                 //Колличество строк массиве
-#define ARRAY_COLLUMN 10                            //Длина строк массива
+#define ARRAY_ROW_COUNT 5                                 //Колличество строк массиве
+#define ARRAY_COLUMN_COUNT 10                            //Длина строк массива
 #define MAX_NUMBER 100                              //Максимально возможный элемент массива
 
-void getRandomArray(int array[ARRAY_ROW][ARRAY_COLLUMN], char rowCount, char collumnCount);
-void printArray(int array[ARRAY_ROW][ARRAY_COLLUMN], char rowCount, char collumnCount);
-int summPositiveElenentsOfArray(int array[ARRAY_ROW][ARRAY_COLLUMN], char rowCount, char collumnCount);
-int randSigned();
+void fillRandomArray(int array[ARRAY_ROW][ARRAY_COLLUMN], char rowCount, char columnCount);
+void printArray(int array[ARRAY_ROW][ARRAY_COLLUMN], char rowCount, char columnCount);
+int summPositiveElenentsOfArray(int array[ARRAY_ROW][ARRAY_COLLUMN], char rowCount, char columnCount);
+int getSignedRandNumber();
 
 int main()
 {
     int arrayIntNumbers[ARRAY_ROW][ARRAY_COLLUMN];  //целочисленный массив
-    getRandomArray(arrayIntNumbers, ARRAY_ROW, ARRAY_COLLUMN);
+    srand(time(0) * 1000);                          //х1000 для более быстрого обновления стартового значения
+    
+    fillRandomArray(arrayIntNumbers, ARRAY_ROW, ARRAY_COLLUMN);
 
     printf("Random array %ix%i:\n", ARRAY_ROW, ARRAY_COLLUMN);
     printArray(arrayIntNumbers, ARRAY_ROW, ARRAY_COLLUMN);
@@ -28,9 +30,8 @@ int main()
 }
 
 /*Заполнение массива случайными числами*/
-void getRandomArray(int array[ARRAY_ROW][ARRAY_COLLUMN], char rowCount, char collumnCount)
+void fillRandomArray(int array[ARRAY_ROW][ARRAY_COLLUMN], char rowCount, char collumnCount)
 {
-    srand(time(0) * 1000);                          //х1000 для более быстрого обновления стартового значения
     for(char row = 0; row < rowCount; row++)
     {
         for(char collumn = 0; collumn < collumnCount; collumn++)
